330acdde6c29bcb653b9d7b5d5d7c295390ea644
Sebastian Garces <sgarcesc@gmail.com>
2017-04-11 23:21:01 -0500

    Yield Return vs New List

---
+++ Benchmarks/YieldVsNewBenchmark.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using System.Collections.Generic;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class YieldVsNewBechmark
+    {
+        [Params(100, 1000, 10000, 100000, 1000000)]
+        public int Elements;
+
+        [Benchmark]
+        public IEnumerable<int> Yiel()
+        {
+            for (var i = 0; i < Elements; i++)
+            {
+                yield return i;
+            }
+        }
+
+        [Benchmark]
+        public IEnumerable<int> New()
+        {
+            var list = new List<int>();
+            for (var i = 0; i < Elements; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+    }
+}
